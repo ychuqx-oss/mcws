be44bce78ea842931dae847a958a99e34391712e
ychuqx-oss <ychuqx@gmail.com>
2026-07-05 02:17:58 +0800

    feat: include 2023 early summer miComet clips

--- src/data/timeline/index.ts
+++ src/data/timeline/index.ts
@@ -2,6 +2,7 @@ import timelineData from './timeline.json';
 import timeline2022BbqData from './timeline-2022-bbq.json';
 import timeline2023EarlyBbqData from './timeline-2023-early-bbq.json';
 import timeline2023SpringBbqData from './timeline-2023-spring-bbq.json';
+import timeline2023EarlySummerBbqData from './timeline-2023-early-summer-bbq.json';
 import timeline2023WinterBbqData from './timeline-2023-winter-bbq.json';
 import timeline2024BbqData from './timeline-2024-bbq.json';
 import timeline2024SpringBbqData from './timeline-2024-spring-bbq.json';
@@ -28,22 +29,37 @@ export interface MiCometStory {
   link?: string;
 }
 
-function normalizeUiText(value?: string) {
+function normalizeZhText(value?: string) {
   if (!value) return value;
   return value
     .replace(/咪口/g, 'Miko')
+    .replace(/35/g, 'Miko')
     .replace(/彗醬/g, '星街')
-    .replace(/\b35\b/g, 'Miko')
-    .replace(/\bsuisei\b/gi, '星街');
+    .replace(/星街彗星/g, '星街')
+    .replace(/櫻巫女/g, 'Miko')
+    .replace(/さくらみこ/g, 'Miko')
+    .replace(/みこち/g, 'Miko')
+    .replace(/すいちゃん/g, '星街')
+    .replace(/星街すいせい/g, '星街')
+    .replace(/\bsuisei\b/gi, '星街')
+    .replace(/Suisei/g, '星街')
+    .replace(/英文熟肉：/g, '')
+    .replace(/日文切り抜き：/g, '')
+    .replace(/日文手描き：/g, '手描き：')
+    .replace(/原直播：/g, '直播：')
+    .replace(/，YT：/g, '。YT：')
+    .replace(/！！！！！/g, '！')
+    .replace(/www/g, '')
+    .replace(/ｗ/g, '')
+    .replace(/  +/g, ' ')
+    .trim();
 }
 
 function normalizeStory(story: MiCometStory): MiCometStory {
   return {
     ...story,
-    title: normalizeUiText(story.title) ?? story.title,
-    titleZh: normalizeUiText(story.titleZh),
-    ctx: normalizeUiText(story.ctx) ?? story.ctx,
-    ctxZh: normalizeUiText(story.ctxZh),
+    titleZh: normalizeZhText(story.titleZh),
+    ctxZh: normalizeZhText(story.ctxZh),
   };
 }
 
@@ -52,6 +68,7 @@ export const MICOMET_TIMELINE: MiCometStory[] = [
   ...(timeline2022BbqData as MiCometStory[]),
   ...(timeline2023EarlyBbqData as MiCometStory[]),
   ...(timeline2023SpringBbqData as MiCometStory[]),
+  ...(timeline2023EarlySummerBbqData as MiCometStory[]),
   ...(timeline2023WinterBbqData as MiCometStory[]),
   ...(timeline2024BbqData as MiCometStory[]),
   ...(timeline2024SpringBbqData as MiCometStory[]),
