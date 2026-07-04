d86e5aa05852ec0f9c63afb7287129e36b6a0bbe
ychuqx-oss <ychuqx@gmail.com>
2026-07-04 22:16:49 +0800

    feat: include May June miComet clips

--- src/data/timeline/index.ts
+++ src/data/timeline/index.ts
@@ -2,6 +2,7 @@ import timelineData from './timeline.json';
 import timeline2022BbqData from './timeline-2022-bbq.json';
 import timeline2024BbqData from './timeline-2024-bbq.json';
 import timeline2024SpringBbqData from './timeline-2024-spring-bbq.json';
+import timeline2024EarlySummerBbqData from './timeline-2024-early-summer-bbq.json';
 
 export interface MiCometStory {
   id: string;
@@ -25,6 +26,7 @@ export const MICOMET_TIMELINE: MiCometStory[] = [
   ...(timeline2022BbqData as MiCometStory[]),
   ...(timeline2024BbqData as MiCometStory[]),
   ...(timeline2024SpringBbqData as MiCometStory[]),
+  ...(timeline2024EarlySummerBbqData as MiCometStory[]),
 ].sort((a, b) => {
   const dateCompare = a.date.localeCompare(b.date);
   if (dateCompare !== 0) return dateCompare;
